f4db1763c0bd72ccc8678dd09ebe336f94c1b29f
Fiaritia <rahamisonf@gmail.com>
2024-07-23 17:12:32 +0300

    Création de l'API pour l'inscription.

--- troc-api/src/main/java/mbds/tpt/troc_api/controllers/UsersController.java
+++ troc-api/src/main/java/mbds/tpt/troc_api/controllers/UsersController.java
@@ -6,9 +6,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
@@ -27,5 +32,13 @@ public class UsersController {
 
         Page<Users> users = userService.findUsers(name, role, PageRequest.of(page, size));
         return ResponseEntity.ok(users);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Users> registerUser(@Valid @RequestBody Users user) {
+        Users newUser = userService.registerUser(user.getUsername(), user.getName(), user.getPassword(),
+                user.getEmail(),
+                user.getPhone(), user.getAddress(), user.getRole());
+        return ResponseEntity.ok(newUser);
     }
 }
