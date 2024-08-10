2afe43d43c445c75a3bc3649a4dd178c7838dd15
Fiaritia <rahamisonf@gmail.com>
2024-08-10 19:22:50 +0300

    Création des classes entités et enum nécessaires.

---
+++ troc-api/src/main/java/mbds/tpt/troc_api/entities/ExchangeItems.java
@@ -0,0 +1,79 @@
+package mbds.tpt.troc_api.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import mbds.tpt.troc_api.utils.ItemRole;
+
+@Entity
+@Table(name = "exchange_items")
+public class ExchangeItems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "exchange_item_id")
+    private Long exchangeItemId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exchange_id", nullable = false)
+    private Exchanges exchange;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Items item;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ItemRole role;
+
+    // Constructeurs
+    public ExchangeItems() {
+    }
+
+    public ExchangeItems(Exchanges exchange, Items item, ItemRole role) {
+        this.exchange = exchange;
+        this.item = item;
+        this.role = role;
+    }
+
+    // Getters et Setters
+    public Long getExchangeItemId() {
+        return exchangeItemId;
+    }
+
+    public void setExchangeItemId(Long exchangeItemId) {
+        this.exchangeItemId = exchangeItemId;
+    }
+
+    public Exchanges getExchange() {
+        return exchange;
+    }
+
+    public void setExchange(Exchanges exchange) {
+        this.exchange = exchange;
+    }
+
+    public Items getItem() {
+        return item;
+    }
+
+    public void setItem(Items item) {
+        this.item = item;
+    }
+
+    public ItemRole getRole() {
+        return role;
+    }
+
+    public void setRole(ItemRole role) {
+        this.role = role;
+    }
+}
---
+++ troc-api/src/main/java/mbds/tpt/troc_api/utils/ExchangeStatus.java
@@ -0,0 +1,8 @@
+package mbds.tpt.troc_api.utils;
+
+public enum ExchangeStatus {
+    EN_ATTENTE,
+    ACCEPTE,
+    REFUSE,
+    TERMINE
+}
