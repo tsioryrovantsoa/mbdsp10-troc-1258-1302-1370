540409dcee98bca180fa2bc9fd5548322915fe5d
mendrikafitahiana <ramilisonfita24@gmail.com>
2024-08-11 11:12:28 +0300

    config date list item

--- troc-api/src/main/java/mbds/tpt/troc_api/configs/JacksonConfig.java
+++ troc-api/src/main/java/mbds/tpt/troc_api/configs/JacksonConfig.java
@@ -1,8 +1,14 @@
 package mbds.tpt.troc_api.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule; // Importez JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,9 +21,15 @@ public class JacksonConfig {
         Hibernate5Module hibernate5Module = new Hibernate5Module();
         hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, false);
 
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, 
+            new LocalDateTimeSerializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
         // Ajoutez le module JSR310 à l'ObjectMapper
-        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(javaTimeModule);
         mapper.registerModule(hibernate5Module);
+
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
         return mapper;
     }
