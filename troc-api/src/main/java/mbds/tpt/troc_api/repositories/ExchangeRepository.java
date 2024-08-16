1a2622049fdf5ce7095dc3a0ded62c85670168c7
tsioryrovantsoa <tsioryrovantsoa@gmail.com>
2024-08-16 21:12:47 +0300

    feat: historique de transaction

--- troc-api/src/main/java/mbds/tpt/troc_api/repositories/ExchangeRepository.java
+++ troc-api/src/main/java/mbds/tpt/troc_api/repositories/ExchangeRepository.java
@@ -1,10 +1,16 @@
 package mbds.tpt.troc_api.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import mbds.tpt.troc_api.entities.Exchanges;
 
 @Repository
 public interface ExchangeRepository extends JpaRepository<Exchanges, Long> {
+    @Query("SELECT e FROM Exchanges e JOIN e.exchangeItems ei WHERE ei.item.item_id = :itemId")
+    List<Exchanges> findExchangesByItemId(@Param("itemId") Long itemId);
 }
