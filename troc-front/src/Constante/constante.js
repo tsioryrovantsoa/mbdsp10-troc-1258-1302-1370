73e762cb5bcaf8f1d91fb9aa6f937c0339925e9b
tsioryrovantsoa <tsioryrovantsoa@gmail.com>
2024-08-31 22:28:58 +0300

    change url

--- troc-front/src/Constante/constante.js
+++ troc-front/src/Constante/constante.js
@@ -1,3 +1,3 @@
 
-export const API_URL = 'http://localhost:8080/';
-export const EXPRESS_API_URL = 'http://localhost:3001/';
+export const API_URL = 'https://mbdsp10-troc-1258-1302-1370-spring.onrender.com/';
+export const EXPRESS_API_URL = 'https://mbdsp10-troc-1258-1302-1370-a68a.onrender.com/';
